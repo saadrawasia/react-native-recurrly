fa334bfb3eb1eed9991d735c708225e943fc3c4b
Saad Rawasia <saad.rawasia15@gmail.com>
2026-07-12 14:50:07 +0200

    Basic Routing & Navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
@@ -7,6 +8,16 @@ export default function App() {
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+      <Link href={'/onboarding'} className="mt-4 rounded bg-primary text-white p-4">Go to onboarding</Link>
+      <Link href={'/(auth)/sign-in'} className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
+      <Link href={'/(auth)/sign-up'} className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
+
+      <Link href={'/subscriptions/spotify'} className="mt-4 rounded bg-primary text-white p-4">Spotify Subscription</Link>
+      <Link href={{
+        pathname: '/subscriptions/[id]', params: {
+          id: 'claude'
+        }
+      }} className="mt-4 rounded bg-primary text-white p-4">Claude Max Subscription</Link>
     </View>
   );
 }
